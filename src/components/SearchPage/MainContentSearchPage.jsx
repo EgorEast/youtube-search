18814a50fc2def3d26d4484268de65dbe324f188
EgorEast <jir.jir35@gmail.com>
2021-07-23 13:01:59 +0300

    main content for search page is added

---
+++ src/components/SearchPage/MainContentSearchPage.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import SearchField from './SearchField';
+import FilterPanel from './FilterPanel';
+import CardVideo from './CardVideo';
+import { Typography } from 'antd';
+
+const { Title } = Typography;
+
+const MainContentSearchPage = () => {
+	const [isSearch, setIsSearch] = useState(false);
+	const displayResultsContainer = () => {
+		if (!isSearch) return { display: 'none' };
+	};
+	return (
+		<div className='search-page-main'>
+			<div className='search-page-main-content'>
+				<div className='search-container'>
+					<Title>Поиск видео</Title>
+					<SearchField setIsSearch={setIsSearch} isSearch={isSearch} />
+				</div>
+				<div className='results-container' style={displayResultsContainer()}>
+					<FilterPanel />
+					<div className='results'>
+						<CardVideo />
+						<CardVideo />
+						<CardVideo />
+						<CardVideo />
+						<CardVideo />
+						<CardVideo />
+						<CardVideo />
+						<CardVideo />
+						<CardVideo />
+						<CardVideo />
+						<CardVideo />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default MainContentSearchPage;
