5ac3ef95ae03558be2c3eda307d03c7640ef1365
EgorEast <jir.jir35@gmail.com>
2021-07-23 18:59:20 +0300

    add json file with list of users and added him in localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import LoginPage from './components/LoginPage';
+// import SearchPage from './components/SearchPage';
+import users from './users.json';
 
 function App() {
-	return <div className=''></div>;
+	useEffect(() => {
+		localStorage.setItem('users', JSON.stringify(users));
+	}, []);
+	return (
+		<div>
+			<LoginPage />
+			{/* <SearchPage /> */}
+		</div>
+	);
 }
 
 export default App;
